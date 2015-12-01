19869f0b8fa37f964ab31bba269d927117be9113
vanderelm <elmvvl@gmail.com>
2015-12-01 19:30:22 +0300

    projects

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+	
+	//Вывод проектов по группам
+
+	$('.group').click(function(e){
+		e.preventDefault();
+		$('.group').each(function(){
+			$(this).removeClass('active');
+		});
+		$(this).addClass('active');
+		var proj_group = $(this).children('.title').text();
+		//console.log(tv);
+		var data = {
+            "proj_group" : proj_group
+        	};
+		$.ajax({ 
+		   type: 'POST', 
+		   url: 'scp.html', 
+		   dataType: 'json', 
+		   data: data, 
+	       beforeSend: function(data) { // событие до отправки
+	            
+	          },
+	       success: function(data){ // событие после удачного обращения к серверу и получения ответа
+	       		$('.projects_getjs').html(data['acc']);
+	       		convas_get_all();
+	         },
+	       error: function (xhr, ajaxOptions, thrownError) { // в случае неудачного завершения запроса к серверу
+	            alert(xhr.status); // покажем ответ сервера
+	            alert(thrownError); // и текст ошибки
+	         },
+	       complete: function(data) { // событие после любого исхода
+
+	         }       
+		});
+	});
+
+
+
+	// функция отрисовки конвасов =)
+	function convas_get_all() {
+			var ar = document.getElementsByClassName('canvas_rt');
+			for(var i in ar) {
+				console.log(ar[i]);
+				var rating = ar[i].getAttribute("data-rating");
+				var rating_cof = eval(((rating / 100)*2) - 0.5);
+				var c = ar[i];
+				var ctx = c.getContext("2d");
+				ctx.beginPath();
+				ctx.lineWidth = 3;
+				ctx.strokeStyle = "rgb(255,199,18)";
+				ctx.arc(40,40,35,-0.5*Math.PI,rating_cof*Math.PI);
+				ctx.stroke();
+			}
+		}
+		convas_get_all();
+});
--- snippets/ajax_bk.php
+++ snippets/ajax_bk.php
@@ -1,1 +1,10 @@
-<?php
+<?php 
+if(isset($_POST['proj_group'])){
+	$proj_group = $_POST['proj_group'];
+	$out = $modx->runSnippet('getResources', array('parents'=>'12','tpl'=>'project_item', 'limit' => '8','includeTVs' => '1', 'processTVs' => '1',  'sortdirTV' => 'DESC', 'tvFilters' => 'project_cat ==%'.$proj_group.'%'));
+	$json['acc'] = $out;
+	echo json_encode($json);
+}
+
+
+?> 
