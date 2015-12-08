09f8090daa4fe05909b062e0556adb50254efdac
vanderelm <elmvvl@gmail.com>
2015-12-08 10:12:56 +0300

    пн

--- snippets/invest_percent.php
+++ snippets/invest_percent.php
@@ -13,11 +13,21 @@ if($type == 'percent2'){
 	$percent = round($price_now*100/$priceneed);
 	echo $percent;
 }
+if($type == 'percent3'){
+	$res = $modx->getObject('modResource',$id);
+	$priceneed = $res->getTVValue('invest_priceneed');
+	$price_now = $res->getTVValue('invest_price_now');
+	$percent = round($price_now*100/$priceneed);
+	echo $percent*0.75;
+}
 if($type == 'line'){
 	echo round(263*$percent/100);
 }
 if($type == 'line2'){
 	echo round(263*$percent/100);
+	if (isset($_GET['id'])) {
+		$id = $_GET['id'];
+	}
 	$res = $modx->getObject('modResource',$id);
 	$priceneed = $res->getTVValue('invest_priceneed');
 	$price_now = $res->getTVValue('invest_price_now');
@@ -25,13 +35,13 @@ if($type == 'line2'){
 	echo round(263*$percent/100);
 }
 if($type == 'date'){
-	function  days($day) 
-    { 
-        $a=substr($day,strlen($day)-1,1); 
-        if($a==1) $str="ДЕНЬ"; 
-        if($a==2 || $a==3 || $a==4) $str="ДНЯ"; 
-        if($a==5 || $a==6 || $a==7 || $a==8 || $a==9 || $a==0) $str="ДНЕЙ"; 
-        return $str; 
+	function  days($day)
+    {
+        $a=substr($day,strlen($day)-1,1);
+        if($a==1) $str="ДЕНЬ";
+        if($a==2 || $a==3 || $a==4) $str="ДНЯ";
+        if($a==5 || $a==6 || $a==7 || $a==8 || $a==9 || $a==0) $str="ДНЕЙ";
+        return $str;
     }
 	$end_date = $modx->resource->get('invest_end_date')['1'];
 	$time = date( "Y-m-d H:i:s" , time());
